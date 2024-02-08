6477fbcbafca3a1df02405603dbb126690de3013
IKLANLO <ilandaberea@yahoo.es>
2024-02-08 22:38:02 +0100

    ListItems usando Context, incluidas noticias guardadas en formulario

--- Web_de_noticias/src/components/Form/Form.jsx
+++ Web_de_noticias/src/components/Form/Form.jsx
@@ -2,26 +2,43 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './Form.styles.scss'
 
+
 const Form = () => {
   const [data, setData] = useState({
     name: '',
     info: '',
+    url: ''
   })
   let navigate = useNavigate()
-
+  let localData = JSON.parse(localStorage.getItem('data'))
+  
   const handleInputChange = (event) => {
     setData({...data, [event.target.name]: event.target.value})
   }
-
+  
+  
   const handleSubmit = (event) => {
     event.preventDefault()
-    localStorage.setItem('data', JSON.stringify({
-      name: data.name,
-      info: data.info
-    }))
+    console.log('data: ' + localData);
+
+    if(localData !== null){
+      localData.push({
+        name: data.name,
+        info: data.info,
+        url: data.url
+      })
+    } else {
+      localData = [{
+        name: data.name,
+        info: data.info,
+        url: data.url
+      }]
+    }
+
+    localStorage.setItem('data', JSON.stringify(localData))
     console.log({message: `data stored successfully: `}, {name: data.name,
-     email: data.info})
-    setTimeout(() => {navigate('/')}, 1000)
+     email: data.info}, {url: data.url})
+    setTimeout(() => {navigate('/list')}, 1000)
   }
 
   return (
@@ -30,6 +47,7 @@ const Form = () => {
         <div className='form__data'>
           <input className='form__data__box' placeholder='título' type='text' name='name' value={data.name} onChange={handleInputChange}/>
           <textarea className='form__data__box' rows={10} placeholder='texto' name='info' value={data.info} onChange={handleInputChange}/>
+          <input className='form__data__box' placeholder='url' type='text' name='url' value={data.url} onChange={handleInputChange}/>
         </div>
         <button type='submit'>Submit</button>
       </form>
--- Web_de_noticias/src/components/ListNews/ListNews.jsx
+++ Web_de_noticias/src/components/ListNews/ListNews.jsx
@@ -2,9 +2,12 @@ import { useContext, useEffect } from 'react';
 import './ListNews.styles.scss'
 import { Article } from '../Article/Article';
 import { GlobalContext } from '../../context/GlobalState';
+import { Link } from 'react-router-dom';
 
 const ListNews = () => {
   const { articles, getArticles } = useContext(GlobalContext)
+  const localData = JSON.parse(localStorage.getItem('data'))
+  let myArts
 
   useEffect(() => {
     getArticles()
@@ -17,7 +20,32 @@ const ListNews = () => {
         </div>
     )
   })
-  return <>{article}</>
+
+  if (localData === null) {
+    myArts = () =>{
+      return (
+        <></>
+      )
+    }
+  } else {
+    myArts = localData.map((data) => {
+        {data.url = 'http://' + data.url}
+        return (
+          <div className='article-container'>
+            <h5 key={data.name}>{data.name}</h5>
+            <p className='article-text'>{data.info}</p>
+            <Link to={data.url}>{data.url}</Link>
+          </div>
+        )  
+    })
+  }
+
+  return (
+    <>
+      {article}
+      {myArts}
+    </>
+  )
 }
 
 export default ListNews
