3851fec02a1bdfbc91800a7e694e03f6529a1cf3
IKLANLO <ilandaberea@yahoo.es>
2024-02-10 10:56:23 +0100

    añadida position sticky a Header

--- Web_de_noticias/src/components/Header/Header.jsx
+++ Web_de_noticias/src/components/Header/Header.jsx
@@ -3,12 +3,12 @@ import './Header.styles.scss'
 
 const Header = () => {
   return (
-    <nav>
-      <>
-        <span><Link className='header-link' to='/'>Home</Link></span>
-        <span><Link className='header-link' to='/form'>Subir noticia</Link></span>
-        <span><Link className='header-link' to='/list'>Noticias</Link></span>
-      </>
+    <nav className='header'>
+      <div>
+        <span><Link className='header__link' to='/'>Home</Link></span>
+        <span><Link className='header__link' to='/form'>Subir noticia</Link></span>
+        <span><Link className='header__link' to='/list'>Noticias</Link></span>
+      </div>
     </nav>
   )
 }
